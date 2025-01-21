53ba835c272dbeef83b5b779513a6a6dee770545
Adam <116597309+mlndlesslydev@users.noreply.github.com>
2025-01-21 17:07:01 +0200

    Update infer_gradio.py

--- src/f5_tts/infer/infer_gradio.py
+++ src/f5_tts/infer/infer_gradio.py
@@ -878,7 +878,7 @@ If you're having issues, try converting your reference audio to WAV or MP3, clip
 def main(port, host, share, api, root_path):
     global app
     print("Starting app...")
-    app.queue(api_open=api).launch(server_name=host, server_port=port, share=share, show_api=api, root_path=root_path)
+    app.queue(api_open=api).launch(server_name=host, server_port=port, share=true, show_api=api, root_path=root_path)
 
 
 if __name__ == "__main__":
